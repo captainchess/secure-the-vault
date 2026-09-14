e962e3879674207f72aaa8eb83d18b915016a991
Alex McCleery <mccleeryalex@gmail.com>
2026-09-14 13:38:44 -0400

    Removed a space in message variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const code2 = 100 / 2 - 10;
 
 const code3 = 13 % 2 + 38;
 
-const message = "The vault has been secured. The combination is: ";
+const message = "The vault has been secured. The combination is:";
 
 const codeA = '"' + code1 + '-' + code2 + '-' + code3 + '"';
 
